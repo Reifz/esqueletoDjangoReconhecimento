69a4383607026661356fb21fcb0a8bd241d27086
GustavoMSandoval <gmsandoval2005@gmail.com>
2025-09-18 15:09:23 -0300

    adicionando admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Usuario, LogAcesso 
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "nivel_acesso")
+    search_fields = ("name", "email")
+    list_filter = ("nivel_acesso",)
+
+@admin.register(LogAcesso)
+class LogAcessoAdmin(admin.ModelAdmin):
+    list_display = ("id", "usuario", "result", "date_time")
+    list_filter = ("result", "date_time")
+    search_fields = ("usuario_name", "usuario_email")
