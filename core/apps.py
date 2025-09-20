f8f3a3106fe372f60a12cea01fb89353a23954a0
GustavoMSandoval <gmsandoval2005@gmail.com>
2025-09-20 08:53:43 -0300

    feat: Adicionando configuração de super usuário , apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.contrib.auth import get_user_model
 
+
+def create_superuser(sender, **kwargs):
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="@dmin123"
+        )
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        post_migrate.connect(create_superuser, sender=self)
